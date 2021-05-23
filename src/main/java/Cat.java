41ebdad3ab88e7360cea96f98fc9e20ede1fbcd4
vsharip <Xec_314gaziz>
2021-05-23 15:48:37 +0500

    Удалил Scope в класс Cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,7 +1,3 @@
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Scope("prototype")
 public class Cat {
     public void catSay() {
         System.out.println("Мяу - мяу");
